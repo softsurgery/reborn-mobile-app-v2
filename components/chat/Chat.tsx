110ffd3573bc18f806e3bfae4dad09edc078472f
Houssem Eddine Chibouni <109615722+softsurgery@users.noreply.github.com>
2025-02-01 23:00:15 +0100

    feat(refetch chat) - add scroll refetch to chat messages

--- components/chat/Chat.tsx
+++ components/chat/Chat.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { ScrollView, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ScrollView, View, ActivityIndicator, RefreshControl, NativeSyntheticEvent, NativeScrollEvent } from "react-native";
 import { Text } from "../ui/text";
 import { Input } from "../ui/input";
 import { IconWithTheme } from "~/lib/IconWithTheme";
 import { Search } from "lucide-react-native";
 import { MessageCard } from "./MessageCard";
-import { StableScrollView } from "../common/StableScrollView";
 import { Separator } from "../ui/separator";
 import { useContextUsers } from "~/hooks/useUsers";
 import { UserBubble } from "./UserBubble";
-import { Button } from "../ui/button";
+import { Loader } from "../Loader";
 
 const getRandomBoolean = () => Math.ceil(Math.random() * 3) === 1;
+
 export const Chat = () => {
   const { users, isFetchingUsers, refetchUsers } = useContextUsers("messages");
 
-  React.useEffect(() => {
+  useEffect(() => {
     refetchUsers();
   }, []);
 
@@ -23,39 +23,22 @@ export const Chat = () => {
     <View className="flex-1 px-5 mb-2">
       <Text className="text-4xl font-bold pb-1">Chat</Text>
       <View className="border-t border-gray-100 dark:border-gray-900 mx-1 gap-2" />
+
       {/* Search Input */}
       <View className="flex flex-row justify-between items-center w-full pt-2">
         <View className="flex flex-row items-center w-full border-hidden rounded py-1">
           <IconWithTheme icon={Search} size={24} />
-          <Input
-            placeholder="Search message..."
-            placeholderTextColor="#9ca3af"
-            className="flex-1 "
-          />
+          <Input placeholder="Search message..." placeholderTextColor="#9ca3af" className="flex-1" />
         </View>
       </View>
 
       {/* List of active people */}
       <View>
-        <Text className="text-gray-800 dark:text-gray-200 text-sm">
-          List of active people
-        </Text>
-        {/* Avatar Row (Static) */}
+        <Text className="text-gray-800 dark:text-gray-200 text-sm">List of active people</Text>
         <View className="-mx-5">
-          <ScrollView
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            className="my-5 px-4"
-          >
-            {/* Static Users */}
+          <ScrollView horizontal showsHorizontalScrollIndicator={false} className="my-5 px-4">
             {users.map((user) => (
-              <UserBubble
-                key={user.uid}
-                className="mx-1.5"
-                label={user.surname}
-                uid={user.uid}
-                gender={user.isMale}
-              />
+              <UserBubble key={user.uid} className="mx-1.5" label={user.surname} uid={user.uid} gender={user.isMale} />
             ))}
           </ScrollView>
         </View>
@@ -63,28 +46,25 @@ export const Chat = () => {
 
       <Separator />
 
-      {/* List of messages */}
-      <View className="border-t border-gray-100 dark:border-gray-900 mx-1 gap-2" />
-      {/* User Cards */}
-      <StableScrollView>
-        {/* Static Users */}
+      {/* Messages List */}
+      <ScrollView 
+        showsVerticalScrollIndicator={false} 
+        refreshControl={
+          <RefreshControl refreshing={isFetchingUsers} onRefresh={refetchUsers}/>
+        }
+      >
         <View className="flex flex-col gap-2 py-2">
           {users.map((user) => (
-            <MessageCard
-              key={user.uid}
-              user={user}
-              latestMessage={"Latest Message"}
-              sentAt={"12:55"}
-              isSeen={getRandomBoolean()}
+            <MessageCard 
+              key={user.uid} 
+              user={user} 
+              latestMessage="Latest Message" 
+              sentAt="12:55" 
+              isSeen={getRandomBoolean()} 
             />
           ))}
         </View>
-      </StableScrollView>
-      {/* <Button onPress={() => refetchUsers()}>
-        <Text className="dark:text-black text-white">
-          refetchUsers {isFetchingUsers ? "Yes" : "No"}
-        </Text>
-      </Button> */}
+      </ScrollView>
     </View>
   );
 };
