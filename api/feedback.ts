fbc8c4f40a298adf368c5ac899ab128e49dab16b
mAmineSouissi <mohamedamine.souissi@esprit.tn>
2025-04-16 00:42:13 +0100

    chore: - remove console.log & change the api url in feedback.ts

--- api/feedback.ts
+++ api/feedback.ts
@@ -2,8 +2,7 @@ import { Feedback } from "~/types";
 import axios from "./axios";
 
 async function postFeedback(feedback: Feedback): Promise<Feedback> {
-  console.log(process.env.EXPO_PUBLIC_API_BASE_URL);
-  const response = await axios.post("/api/feedbacks", feedback);
+  const response = await axios.post("api/client/feedback", feedback);
   return response.data;
 }
 
