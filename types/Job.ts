db7d942afb4d8e1390fb6513a582bb960ca2ae68
Mohamed Amine Souissi <131789599+mAmineSouissi@users.noreply.github.com>
2025-08-18 23:33:06 +0100

    chore: list job properties

--- types/Job.ts
+++ types/Job.ts
@@ -1,20 +1,21 @@
-// export interface Job {
-//   id: string;
-//   title: string;
-//   price: string;
-//   proposals: string;
-//   paymentVerified: boolean;
-//   spent: string;
-//   description: string;
-//   tags: string[];
-//   postedAgo: string;
-//   clientName?: string;
-//   clientRating?: number;
-//   clientLocation?: string;
-//   clientJobsPosted?: number;
-//   clientHireRate?: number;
-//   skillsRequired?: string[];
-//   projectType?: "Fixed" | "Hourly";
-//   duration?: string;
-//   experienceLevel?: "Entry" | "Intermediate" | "Expert";
-// }
+// Job Posters 
+export interface Job {
+  //!id: string;
+  //!title: string;
+  //!price: string;
+  //!description: string;
+  //!tags: string[];
+  // !postedAgo: string;
+  //!clientLocation?: string; // region
+  skillsRequired?: string[];
+  proposals: string; //? nested
+  paymentVerified: boolean; //? nested
+  spent: string; //? nested
+  clientName?: string;
+  clientRating?: number;
+  clientJobsPosted?: number; //* statistics
+  clientHireRate?: number; //* statistics
+  projectType?: "Fixed" | "Hourly";
+  duration?: string;
+  experienceLevel?: "Entry" | "Intermediate" | "Expert"; // Rating (extra)
+}
