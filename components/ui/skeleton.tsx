12339ef03c5852224ee082a5ccaa2e6f14336687
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2025-08-26 14:10:36 +0100

    feat(*): update skeleton component

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,36 +1,13 @@
-import * as React from 'react';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-  withRepeat,
-  withSequence,
-  withTiming,
-} from 'react-native-reanimated';
-import { cn } from '~/lib/utils';
-
-const duration = 1000;
+import { View } from "react-native";
+import { cn } from "~/lib/utils";
 
 function Skeleton({
   className,
   ...props
-}: Omit<React.ComponentPropsWithoutRef<typeof Animated.View>, 'style'>) {
-  const sv = useSharedValue(1);
-
-  React.useEffect(() => {
-    sv.value = withRepeat(
-      withSequence(withTiming(0.5, { duration }), withTiming(1, { duration })),
-      -1
-    );
-  }, []);
-
-  const style = useAnimatedStyle(() => ({
-    opacity: sv.value,
-  }));
-
+}: React.ComponentProps<typeof View> & React.RefAttributes<View>) {
   return (
-    <Animated.View
-      style={style}
-      className={cn('rounded-md bg-secondary dark:bg-muted', className)}
+    <View
+      className={cn("bg-accent animate-pulse rounded-md", className)}
       {...props}
     />
   );
