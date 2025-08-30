9ff3872e59f13ffbc31f8a48c6613ebd20aa191c
Mohamed Amine Souissi <131789599+mAmineSouissi@users.noreply.github.com>
2025-08-30 19:34:03 +0100

    chore: change skeleton color

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -7,7 +7,7 @@ function Skeleton({
 }: React.ComponentProps<typeof View> & React.RefAttributes<View>) {
   return (
     <View
-      className={cn("bg-accent animate-pulse rounded-md", className)}
+      className={cn("bg-primary/25 animate-pulse rounded-md", className)}
       {...props}
     />
   );
