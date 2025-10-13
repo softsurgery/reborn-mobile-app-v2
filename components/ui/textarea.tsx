b0c03d50afe85721577861ab9ea8972e2221ba49
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2025-10-13 11:46:23 +0100

    fix(textarea): repair placeholder color

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -20,9 +20,10 @@ const Textarea = React.forwardRef<
       <TextInput
         ref={ref}
         className={cn(
-          "text-foreground border-input dark:bg-input/30 flex min-h-16 w-full flex-row rounded-md border bg-transparent px-3 py-2 text-base shadow-sm shadow-black/5 md:text-sm",
+          "text-foreground placeholder:text-muted-foreground selection:text-primary-foreground border-input dark:bg-input/30 flex min-h-16 w-full flex-row rounded-md border bg-transparent px-3 py-2 text-base shadow-sm shadow-black/5 md:text-sm",
           Platform.select({
             web: "placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive field-sizing-content resize-y outline-none transition-[color,box-shadow] focus-visible:ring-[3px] disabled:cursor-not-allowed",
+            native: "placeholder:text-muted-foreground/50",
           }),
           props.editable === false && "opacity-50",
           className
