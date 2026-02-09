33aab02cbfbddbfc99d03579432e6bae37e470f7
Mohamed Amine Souissi <131789599+mAmineSouissi@users.noreply.github.com>
2026-02-09 17:20:07 +0100

    feat(experience): initialize Crud experience functionalities with validation

---
+++ components/account/experience/CreateExperience.tsx
@@ -0,0 +1,99 @@
+import { api } from "~/api";
+import { ApplicationHeader } from "~/components/shared/AppHeader";
+import { FormBuilder } from "~/components/shared/form-builder/FormBuilder";
+import { StableKeyboardAwareScrollView } from "~/components/shared/StableKeyboardAwareScrollView";
+import { StableSafeAreaView } from "~/components/shared/StableSafeAreaView";
+import { Button } from "~/components/ui/button";
+import { Text } from "~/components/ui/text";
+import { cn } from "~/lib/utils";
+import { useUserStore } from "~/hooks/stores/useUserStore";
+import { CreateExperienceDto, ServerErrorResponse } from "~/types";
+import { createExperienceSchema } from "~/types/validations/experience.validation";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { router } from "expo-router";
+import { ArrowLeft } from "lucide-react-native";
+import { useTranslation } from "react-i18next";
+import { View } from "react-native";
+import { showToastable } from "react-native-toastable";
+import { useCreateExperienceFormStructure } from "./useCreateExperienceFormStructure";
+
+interface CreateExperienceProps {
+  className?: string;
+}
+
+export const CreateExperience = ({ className }: CreateExperienceProps) => {
+  const { t } = useTranslation("common");
+  const userStore = useUserStore();
+  const queryClient = useQueryClient();
+
+  const { structure } = useCreateExperienceFormStructure({
+    store: userStore,
+  });
+
+  const { mutate: createExperience } = useMutation({
+    mutationFn: (data: { id: string; experience: CreateExperienceDto }) =>
+      api.experience.create(data.id, data.experience),
+    onSuccess: () => {
+      showToastable({
+        message: "Experience created successfully",
+        status: "success",
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["experiences", userStore.response?.id],
+      });
+      router.back();
+    },
+    onError: (error: ServerErrorResponse) => {
+      showToastable({ message: error.response?.data?.message });
+    },
+  });
+
+  const handleCreateSubmit = () => {
+    const data = userStore.createExperienceDto;
+    const result = createExperienceSchema.safeParse(data);
+    if (!result.success) {
+      userStore.set("experienceErrors", result.error.flatten().fieldErrors);
+    } else {
+      if (userStore.response?.id) {
+        createExperience({
+          id: userStore.response?.id!,
+          experience: data,
+        });
+      }
+    }
+  };
+
+  return (
+    <StableSafeAreaView className={cn("flex-1", className)}>
+      <ApplicationHeader
+        className="border-b border-border pb-2 bg-transparent"
+        title={t("screens.experience")}
+        titleVariant="large"
+        reverse
+        shortcuts={[
+          {
+            key: "back",
+            icon: ArrowLeft,
+            onPress: () => router.back(),
+          },
+        ]}
+      />
+
+      {/* Scrollable content */}
+      <StableKeyboardAwareScrollView className="flex-1 bg-background">
+        <FormBuilder structure={structure} className="mt-4 px-2" />
+      </StableKeyboardAwareScrollView>
+
+      {/* Sticky bottom button */}
+      <View className="py-6 border-t border-border">
+        <Button
+          size="sm"
+          className="mx-6 mb-4 rounded-full"
+          onPress={handleCreateSubmit}
+        >
+          <Text>Create Experience</Text>
+        </Button>
+      </View>
+    </StableSafeAreaView>
+  );
+};
