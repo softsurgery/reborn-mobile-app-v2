05103ca38987837e4a26b0472a6dbfe9d5bf99e6
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-03-16 22:36:54 +0000

    feat(tailwind): add custom font sizes to tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,6 +50,16 @@ module.exports = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+       fontSize: {
+        xs: ["10px", "14px"],
+        sm: ["12px", "16px"],
+        base: ["14px", "20px"],
+        lg: ["16px", "24px"],
+        xl: ["18px", "28px"],
+        "2xl": ["20px", "28px"],
+        "3xl": ["24px", "32px"],
+        "4xl": ["30px", "36px"],
+      },
       borderWidth: {
         hairline: hairlineWidth(),
       },
