8bccf01742f57c88bf26c37dea6dfe351d92e0d2
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-08-20 13:23:46 +0000

    feat(profile): add RTL support and update layout dimensions for BaseProfileSkeleton

--- components/profile/BaseProfileSkeleton.tsx
+++ components/profile/BaseProfileSkeleton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View } from "react-native";
 import { Skeleton } from "@/components/ui/skeleton";
 import { cn } from "@/lib/utils";
+import { useRTL } from "~/hooks/useRTL";
 
 interface BaseProfileSkeletonProps {
   className?: string;
@@ -10,15 +11,28 @@ interface BaseProfileSkeletonProps {
 export const BaseProfileSkeleton = ({
   className,
 }: BaseProfileSkeletonProps) => {
+  const isRTL = useRTL();
+
   return (
     <View className={cn("flex-1 bg-background", className)}>
-      {/* Cover */}
-      <Skeleton className="h-48 w-full rounded-none bg-primary/25" />
+      {/* Cover Skeleton */}
+      <Skeleton className="h-56 w-full rounded-none bg-primary/25" />
 
-      {/* Header */}
-      <View className="-mt-12 px-5">
-        <View className="flex-row items-end justify-between">
-          <View className="rounded-full border-4 border-background">
+      {/* Header Container */}
+      <View
+        className={cn(
+          "flex-col px-4 z-10 -mt-12",
+          isRTL ? "items-end" : "items-start",
+        )}
+      >
+        {/* Avatar */}
+        <View
+          className={cn(
+            "w-full justify-between",
+            isRTL ? "flex-row-reverse" : "flex-row",
+          )}
+        >
+          <View className="rounded-full border-4 border-background bg-background">
             <Skeleton className="h-[100px] w-[100px] rounded-full" />
           </View>
         </View>
