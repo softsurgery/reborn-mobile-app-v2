8682a6a7de55092ba41d60c72b8b99628ec62793
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-08-23 22:55:06 +0000

    ref: replace chat stack screen with direct conversation route in main layout

--- app/main/_layout.tsx
+++ app/main/_layout.tsx
@@ -377,13 +377,7 @@ export default function MainLayout() {
         }}
       />
       {/* Chat  ********************************************************************************************* */}
-      <Stack.Screen
-        name="chat"
-        options={{
-          title: "chat",
-          headerShown: false,
-        }}
-      />
+
       <Stack.Screen
         name="chat/conversation"
         options={{
