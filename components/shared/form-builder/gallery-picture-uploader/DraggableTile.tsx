5aac8504c33f7a0d5e3905435d19f03fd1e2d5ab
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2026-08-28 10:52:25 +0000

    style(form-builder): enhance draggable title design & tailwind usage

--- components/shared/form-builder/gallery-picture-uploader/DraggableTile.tsx
+++ components/shared/form-builder/gallery-picture-uploader/DraggableTile.tsx
@@ -4,6 +4,7 @@ import { Image } from "expo-image";
 import { X } from "lucide-react-native";
 import { ImageFile } from "../types";
 import { cn } from "@/lib/utils";
+import { useColorPalette } from "@/hooks/useColorPalette";
 
 interface DraggableTileProps {
   className?: string;
@@ -18,19 +19,17 @@ export const DraggableTile = ({
   size,
   onRemove,
 }: DraggableTileProps) => {
+  const { palette } = useColorPalette();
   return (
     <View
       className={cn(
-        "rounded-lg overflow-hidden bg-gray-200",
+        "rounded-t-xl overflow-hidden border border-border",
         item.progress < 100 && "opacity-70",
         className,
       )}
       style={{
         width: size,
         height: size,
-        borderRadius: 10,
-        overflow: "hidden",
-        backgroundColor: "#f0f0f0",
       }}
     >
       <Image
@@ -38,35 +37,17 @@ export const DraggableTile = ({
         style={{ width: "100%", height: "100%" }}
         contentFit="cover"
       />
-
       {/* X button */}
-      <Pressable
-        onPress={onRemove}
-        style={{
-          position: "absolute",
-          top: 6,
-          right: 6,
-          backgroundColor: "rgba(0,0,0,0.5)",
-          borderRadius: 12,
-          padding: 2,
-        }}
-      >
-        <X size={16} color="white" />
+      <Pressable onPress={onRemove} className="absolute top-0 right-0 p-2">
+        <X size={16} color={palette.foreground} />
       </Pressable>
-
       {/* Progress bar */}
-      {item.progress < 100 && (
-        <View
-          style={{
-            position: "absolute",
-            bottom: 0,
-            left: 0,
-            height: 3,
-            width: `${item.progress}%`,
-            backgroundColor: "#22c55e",
-          }}
-        />
-      )}
+      <View
+        className=" absolute bottom-0 left-0 h-1 bg-primary"
+        style={{
+          width: `${item.progress}%`,
+        }}
+      />
     </View>
   );
 };
