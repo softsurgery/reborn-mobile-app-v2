d8e662bad629524a01179edd442fd779b41f56e1
Mohamed Amine Souissi <131789599+mAmineSouissi@users.noreply.github.com>
2026-09-01 11:08:46 +0100

    feat(request): add request desision action sheets

--- components/jobs/requests/details/RequestDetails.tsx
+++ components/jobs/requests/details/RequestDetails.tsx
@@ -22,7 +22,9 @@ import { RequestBanner } from "./RequestBanner";
 import { RequestUserEntry } from "./RequestUserEntry";
 import { RequestJobEntry } from "./RequestJobEntry";
 import { RequestApplicationDetails } from "./RequestApplicationDetails";
-import { RequestDecisions } from "./RequestDecisions";
+import { RequestDecisionsActionSheet } from "./RequestDecisionsActionSheet";
+import { ActionSheetRef } from "react-native-actions-sheet";
+import { BottomButtonWrapper } from "@/components/shared/BottomButtonBlockWrapper";
 
 export interface RequestStatus {
   icon: LucideIcon;
@@ -40,6 +42,7 @@ interface RequestDetailsProps {
 
 export const RequestDetails = ({ className, id }: RequestDetailsProps) => {
   const { currentUser } = useCurrentUser();
+  const decisionsSheetRef = React.useRef<ActionSheetRef>(null);
 
   const {
     request,
@@ -174,16 +177,30 @@ export const RequestDetails = ({ className, id }: RequestDetailsProps) => {
 
           {/* Application Details */}
           <RequestApplicationDetails className="px-4" request={request} />
+          {/* Decisions Block is now inside BottomButtonWrapper below */}
 
-          {/* Decisions Block */}
-          <RequestDecisions
-            className="px-4 mt-4"
+
+          <RequestDecisionsActionSheet
+            ref={decisionsSheetRef}
+            className="mt-4"
             request={request}
             isIncoming={isIncoming}
             refetch={refetch}
           />
         </View>
       </ScrollView>
+      <BottomButtonWrapper>
+        <Button
+          onPress={() => decisionsSheetRef.current?.show()}
+          variant="default"
+          size="lg"
+          className="w-full rounded-xl h-12"
+        >
+          <Text className="font-bold text-primary-foreground text-md">
+            Manage Application
+          </Text>
+        </Button>
+      </BottomButtonWrapper>
     </StableSafeAreaView>
   );
 };
